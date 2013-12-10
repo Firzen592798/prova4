779f00a797964e66e47c32f672723d15cbb3b852
Firzen592798 <tyranitar_nb@hotmail.com>
2013-12-10 18:09:47 -0300

    comentando a linha do spec do travis.yml

--- test/models/cliente_test.rb
+++ test/models/cliente_test.rb
@@ -3,12 +3,13 @@ require 'test_helper'
 class ClienteTest < ActiveSupport::TestCase
 	test "Verifica se os clientes sem nome eh salvo" do
 	  cliente = Cliente.new
-	  
+	  cliente.CPF ='11122233311'
 	  assert !cliente.save, "Cliente sem nenhum campo preenchido foi salvo"
 	end
 	
 	test "Verifica se os clientes sem CPF eh salvo" do
 	  cliente = Cliente.new
+	  cliente.CPF ='11122233311'
 	  assert !cliente.save, "Cliente sem nenhum campo preenchido foi salvo"
 	end
 end
