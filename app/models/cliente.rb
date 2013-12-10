70cc306d82eb7e74f5aa0bf3579c1ce141d27410
Firzen592798 <tyranitar_nb@hotmail.com>
2013-12-10 19:44:39 -0300

    Implementando algumas validacoes
    build pode quebrar pq agora alguns testes vao dar erro

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,4 +1,11 @@
 class Cliente < ActiveRecord::Base
-validates :Nome, presence: true
-validates :CPF, presence: true
+validates :Nome, :presence => { :message => 'nao pode ser nulo' }
+validates :CPF, :presence => { :message => 'nao pode ser nulo' }
+validates :Email, :presence => { :message => 'nao pode ser nulo' }
+validates :CPF, :length => { :is => 11, :message=>'tem que ter 11 digitos' } 
+validates :CPF, :format => { :with => /\A[+-]?\d+\Z/, :message=>'tem que ter so numeros' } 
+validates :Telefone, :presence => { :message => 'nao pode ser nulo' }
+validates :Telefone, :format => { :with => /\A[+-]?\d+\Z/, :message=>'tem que ter so numeros' } 
+validates :Telefone, :length => { :is => 8, :message=>'tem que ter 8 digitos' } 
+validates :Telefone, :format => { :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/, :message=>'Isso nao parece um email' } 
 end
--- test/models/cliente_test.rb
+++ test/models/cliente_test.rb
@@ -7,9 +7,16 @@ class ClienteTest < ActiveSupport::TestCase
 	  assert !cliente.save, "Cliente sem nenhum campo preenchido foi salvo"
 	end
 	
-	test "Verifica se os clientes sem CPF eh salvo" do
+	test "Verifica se o cliente com CPF e nome eh salvo" do
 	  cliente = Cliente.new
 	  cliente.CPF ='11122233311'
+	  cliente.Nome ='Cliente'
+	  assert !cliente.save, "Cliente com campos preenchidos corretamente nao foi salvo"
+	end
+	
+	test "Verifica se os clientes sem CPF eh salvo" do
+	  cliente = Cliente.new
+	  cliente.CPF ="11122233311"
 	  assert !cliente.save, "Cliente sem nenhum campo preenchido foi salvo"
 	end
 end
